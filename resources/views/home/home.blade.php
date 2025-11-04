b356975399ff233829567292eed562a932197766
unknown <salamjeanlouis3@gmail.com>
2025-11-04 15:52:51 +0000

    fix: preposition

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -11,7 +11,7 @@
       <div class="container position-relative">
 
         <div class="welcome position-relative" data-aos="fade-down" data-aos-delay="100">
-          <h2>BIENVENUE A LA MAIRIE DU PLATEAU</h2>
+          <h2>BIENVENUE À LA MAIRIE DU PLATEAU</h2>
           <p>Vous pouvez désormais faire votre demandes d'actes civils à la mairie du plateau en ligne </p>
         </div><!-- End Welcome -->
 
