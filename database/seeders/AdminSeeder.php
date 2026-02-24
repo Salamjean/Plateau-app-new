3720f1fb6068e59cc84dc738b53e27c4e141f986
unknown <salamjeanlouis3@gmail.com>
2026-02-24 11:43:28 +0000

    fix: parents infos

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -16,7 +16,7 @@ class AdminSeeder extends Seeder
        Admin::create([
             'name' => 'KKS-Technologies',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('azertyui'),
+            'password' => Hash::make('KKStechnologies2022@'),
         ]);
        
     }
